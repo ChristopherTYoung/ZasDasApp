cd51d9f7ca0e5e8f50628b706edda19ffa04f601
Christopher Young <c.t.ythegamer@gmail.com>
2025-03-20 19:29:31 -0600

    change

---
+++ ZasAndDasMobile/Views/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ZasAndDasMobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
