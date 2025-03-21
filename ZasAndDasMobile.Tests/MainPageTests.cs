afce570c3bc82519aebb87c613aa612420f05bb5
JadedRain <darkenedninja450@gmail.com>
2025-03-20 19:22:34 -0600

    First passing test

---
+++ ZasAndDasMobile.Tests/MainPageTests.cs
@@ -0,0 +1,12 @@
+﻿using ZasAndDasMobile.Models;
+namespace ZasAndDasMobile.Tests
+{
+    public class MainPageTests
+    {
+        [Fact]
+        public void CanCreateCart()
+        {
+            var cart = new Cart();
+        }
+    }
+}
