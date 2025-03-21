2be60d025914ec77bd9974d97e40d84869d6811d
pokre <pokreman06@gmail.com>
2025-03-21 12:30:37 -0600

    added minor testing for Pizza service

---
+++ ZasAndDasMobile/Services/PizzaService.cs
@@ -0,0 +1,23 @@
+﻿using Square;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZasUndDas.Shared;
+
+namespace ZasAndDasMobile.Services
+{
+    public class PizzaService
+    {
+        List<Pizza> pizzas = new List<Pizza>();
+        public List<Pizza> GetAllPizzas()
+        {
+            return pizzas.ToList();
+        }
+        public void AddPizza(Pizza pizza)
+        {
+            pizzas.Add(pizza);
+        }
+    }
+}
---
+++ ZasAndDasMobile/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ZasAndDasMobile.ViewModels
+{
+
+    public partial class MainPageViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        string observableObject;
+    }
+}
