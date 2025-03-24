6fd3c2fcfafc9a9941b2451fa2627b2e840a63bc
Christopher Young <c.t.ythegamer@gmail.com>
2025-03-24 12:49:59 -0600

    change

--- ZasAndDasMobile/ViewModels/MainPageViewModel.cs
+++ ZasAndDasMobile/ViewModels/MainPageViewModel.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using ZasAndDasMobile.Services;
 using ZasUndDas.Shared;
 
@@ -18,10 +19,22 @@ namespace ZasAndDasMobile.ViewModels
         public string Rows => $"{DeviceDisplay.Current.MainDisplayInfo.Height - 200}";
 
         [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(ShowPizzasCommand))]
+        [NotifyCanExecuteChangedFor(nameof(ShowDrinksCommand))]
+        private bool pizzasAreVisible;
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof (ShowDrinksCommand))]
+        [NotifyCanExecuteChangedFor(nameof(ShowPizzasCommand))]
+        private bool drinksAreVisible;
+
+        [ObservableProperty]
         private ObservableCollection<PizzaViewModel> pizzaList;
 
         public MainPageViewModel(PizzaService service)
         {
+            PizzasAreVisible = true;
+            DrinksAreVisible = false;
             _service = service;
             Sync();
         }
@@ -30,6 +43,27 @@ namespace ZasAndDasMobile.ViewModels
             PizzaList = new ObservableCollection<PizzaViewModel>();
             foreach (var pizza in _service.GetAllPizzas())
                 PizzaList.Add(new PizzaViewModel(pizza));
+        }
+
+        bool PizzasVis() => !PizzasAreVisible;
+        [RelayCommand(CanExecute = nameof(PizzasVis))]
+        private void ShowPizzas()
+        {
+            PizzasAreVisible = true;
+            DrinksAreVisible = false;
+        }
+        bool DrinksVis() => !DrinksAreVisible;
+
+        [RelayCommand(CanExecute =  nameof(DrinksVis))]
+        private void ShowDrinks()
+        {
+            DrinksAreVisible = true;
+            PizzasAreVisible = false;
+        }
+
+        private void UpdateTabs()
+        {
+            
         }
     }
 }
