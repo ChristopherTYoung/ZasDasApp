03c449c697cb323a430bc893f5fa2e2ea7181f2e
Christopher Young <c.t.ythegamer@gmail.com>
2025-04-16 11:29:58 -0600

    ok this might help

--- SQL/fullConfidenceRestore.sh
+++ SQL/fullConfidenceRestore.sh
@@ -1,11 +1,14 @@
-PGDATABASE='postgres'
-PGUSER='postgres'
-PGPASSWORD='password'
-PGHOST='127.0.0.1'
+export PGDATABASE='postgres'
+export PGUSER='postgres'
+export PGPASSWORD='password'
+export PGHOST='127.0.0.1'
             
 kubectl port-forward svc/zas-db-svc 5432:5432 -n zasanddas &
+
 while ! nc -z localhost 5432; do
     sleep 0.5
 done
 
 psql postgres < zas.sql
+
+kill -9 $(lsof -ti :5432)
