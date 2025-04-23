975b3957dac1a670cae08745c15eca915098aafb
Christopher Young <c.t.ythegamer@gmail.com>
2025-04-23 13:55:58 -0600

    all tests pass

---
+++ ZasAndDasMobile.Tests/APITests.cs
@@ -0,0 +1,59 @@
+﻿using Square.Webhooks.Subscriptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZasAndDasWeb.Controllers;
+using ZasAndDasWeb.Services;
+using Shouldly;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs;
+using Azure;
+using Microsoft.AspNetCore.Http;
+using NSubstitute;
+namespace ZasAndDasMobile.Tests
+{
+    public class APITests
+    {
+        [Fact]
+        public async Task Auth()
+        {
+            var auth = new AuthController(new MockAPIKeyValidationService());
+            (await auth.Create(new ZasUndDas.Shared.Services.CreateRequest() { Name = "jeff", Email = "jeff@gmail.com", PassCode = "jefaf" })).ShouldBe("jeff@gmail.comtest");
+            auth.Authenticate(new ZasUndDas.Shared.Services.AuthRequest() { Email = "jeff@gmail.com", PassCode = "jefaf" }).ShouldBe("jeff@gmail.comtest");
+        }
+
+        [Fact]
+        public async Task CanUploadAndDownloadImages()
+        {
+            var testStream = new MemoryStream(Encoding.UTF8.GetBytes("fake image"));
+            var file = new FormFile(testStream, 0, testStream.Length, "file", "test.png");
+
+            // return object for the response for DownloadAsync
+            var downloadResult = BlobsModelFactory.BlobDownloadInfo(
+                content: testStream,
+                contentType: "image/png"
+            );
+
+            // fakeResponse that DownloadAsync returns
+            var fakeResponse = Substitute.For<Response<BlobDownloadInfo>>();
+            fakeResponse.Value.Returns(downloadResult);
+
+            var blobClient = Substitute.For<BlobClient>();
+            blobClient.DownloadAsync().Returns(Task.FromResult(fakeResponse));
+
+            var containerClient = Substitute.For<BlobContainerClient>();
+            containerClient.GetBlobClient("test.png").Returns(blobClient);
+
+            var blobService = new BlobService(containerClient);
+            var blobController = new BlobController(blobService);
+
+            var response = await blobController.UploadImageAsync(file);
+            response.ShouldBe(Results.Ok());
+
+            var image = await blobController.GetImageAsync("test.png");
+            image.ContentType.ShouldBe("image/png");
+        }
+    }
+}
---
+++ ZasUndDas.Shared/Services/IAPIService.cs
@@ -0,0 +1,17 @@
+﻿using ZasUndDas.Shared.Data;
+namespace ZasUndDas.Shared.Services;
+
+public interface IAPIService
+{
+    public bool LoggedIn { get; }
+    public Task Authorize(AuthRequest request);
+    public Task CreateAccount(CreateRequest request);
+    public Task<List<DrinkBaseDTO>> GetDrinks();
+    public Task<List<PizzaBaseDTO>> GetPizzas();
+    public Task<List<PAddinDTO>> GetPizzaToppings();
+    public Task<List<DAddinDTO>> GetDrinkAddins();
+    public Task<List<PizzaSize>> GetPizzaSizes();
+    public Task<List<Sauce>> GetSauces();
+    public Task Order(OrderDTO order);
+    public Uri? BaseAddress { get; }
+}
