16a6c3617b1c9c7800fa2d2e1f7f266a17925d53
Christopher Young <c.t.ythegamer@gmail.com>
2025-04-24 13:14:30 -0600

    fixing the connectiong string configuration

--- ZasAndDasWeb/Program.cs
+++ ZasAndDasWeb/Program.cs
@@ -42,7 +42,7 @@ public class Program
         builder.Services.AddSingleton<PaymentService>();
         builder.Services.AddMetrics();
         builder.Services.AddControllers();
-        builder.Services.AddDbContext<PostgresContext>(o => o.UseNpgsql(builder.Configuration["DB_CONN"]));
+        builder.Services.AddDbContext<PostgresContext>(o => o.UseNpgsql(builder.Configuration.GetConnectionString("DB_CONN") ?? builder.Configuration["DB_CONN"]));
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
         builder.Services.AddScoped<OrderService>();
@@ -112,7 +112,6 @@ public class Program
         app.MapRazorComponents<App>();
 
         app.Run();
-
     }
 }
 
