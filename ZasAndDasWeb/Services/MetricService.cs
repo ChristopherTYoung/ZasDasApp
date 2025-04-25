0a2ef542348233f31c2e515b1d878f1fcb583cd2
Christopher Young <c.t.ythegamer@gmail.com>
2025-04-24 19:26:32 -0600

    another gauge test

--- ZasAndDasWeb/Services/MetricService.cs
+++ ZasAndDasWeb/Services/MetricService.cs
@@ -6,9 +6,8 @@ namespace ZasAndDasWeb.Services
     {
         public static string MeterName = "zasanddas_meter";
         private readonly ObservableGauge<long> totalMemoryUsed;
-        public MetricService(IMeterFactory meterFactory)
+        public MetricService(Meter meter)
         {
-            var meter = meterFactory.Create(MeterName);
             totalMemoryUsed = meter.CreateObservableGauge<long>("total_memory_used", () => new Measurement<long>(GC.GetTotalMemory(true)));
         }
     }
