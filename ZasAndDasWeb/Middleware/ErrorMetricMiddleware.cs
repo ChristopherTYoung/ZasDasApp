c3e1aaaf577dea2c19aa7a5cd76edde13a50bf7f
pokre <pokreman06@gmail.com>
2025-04-24 21:00:04 -0600

    fixed the DI mishap

--- ZasAndDasWeb/Middleware/ErrorMetricMiddleware.cs
+++ ZasAndDasWeb/Middleware/ErrorMetricMiddleware.cs
@@ -6,9 +6,9 @@ namespace ZasAndDasWeb.Middleware
     public class ErrorMetricMiddleware
     {
         private readonly RequestDelegate _next;
-        private readonly MetricService _myService;
+        private readonly MetricService? _myService;
 
-        public ErrorMetricMiddleware(RequestDelegate next, MetricService myService)
+        public ErrorMetricMiddleware(RequestDelegate next, MetricService? myService)
         {
             _next = next;
             _myService = myService;
@@ -17,7 +17,8 @@ namespace ZasAndDasWeb.Middleware
         public async Task InvokeAsync(HttpContext context)
         {
             await _next(context);
-            _myService.ResponseCodes.Record(context.Response.StatusCode);
+            if (_myService != null)
+                _myService.ResponseCodes.Record(context.Response.StatusCode);
         }
 
     }
